5a88074408d4da170ac75b3554beaaac4c501d23
Sanchez <sanchez.cliff.86@gmail.com>
2021-08-24 11:57:16 -0700

    add nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,97 +1,181 @@
 import styled from 'styled-components'
+import logo from '../../assets/logo.png'
+import { BiUser, BiMap, BiSearch } from 'react-icons/bi'
 const StyledHeader = styled.header`
 	--padding: 1rem;
 	height: 60px;
-	background-color: gray;
+	background-color: white;
+	color: #333;
+
+	button {
+		background-color: var(--brandRed);
+		color: white;
+		text-transform: uppercase;
+		font-weight: bold;
+		border: none;
+		padding: .75rem 1.25rem;
+		border-radius: 9999px;
+		@media (min-width: 768px) {
+			background-color: #fff;
+			color: var(--brandRed);
+		}
+	}
 
 	#wrapper {
-        padding: 0 var(--padding);
+		padding: 0 var(--padding);
 		display: flex;
 		align-items: center;
 		justify-content: space-between;
-		gap: 1rem;
+		gap: 2rem;
 		position: relative;
 		height: 100%;
 		max-width: 1150px;
 		margin: 0 auto;
+		text-transform: capitalize;
 		@media (min-width: 768px) {
-            justify-content: flex-end;
+			justify-content: flex-end;
 		}
-        
-		/* logo */
-		> div:nth-of-type(2) {
-            position: absolute;
-			left: 50%;
-			top: 50%;
-			transform: translate(-50%, -50%);
+		a {
+			text-decoration: none;
+			color: inherit;
+			font-weight: 500;
+			font-size: 90%;
+			display: flex;
+			flex-flow: column;
+			align-items: center;
+			gap: .15rem;
+			opacity: .85;
 			@media (min-width: 768px) {
-                left: var(--padding);
-				transform: translateY(-50%);
+				flex-flow: row;
+			}
+			&:hover {
+				opacity: 1;
+			}
+			svg {
+				font-size: 1.45rem;
+				color: var(--brandRed);
 			}
 		}
-        /* search */
-        >div:nth-of-type(4) {
-            display: none;
-            @media (min-width: 768px) {
-                display: block;
-            }
-        }
+
+		/* logo */
+		> div:nth-of-type(1) {
+			display: flex;
+			align-items: center;
+			gap: 2rem;
+			img {
+				max-width: 70px;
+				filter: brightness(.95);
+			}
+			ul {
+				display: none;
+				font-weight: 500;
+				@media (min-width: 768px) {
+					display: flex;
+					gap: 4rem;
+				}
+				&:first-of-type {
+					@media (min-width: 1280px) {
+						display: none;
+					}
+				}
+				&:last-of-type {
+					display: none;
+					@media (min-width: 1280px) {
+						display: flex;
+					}
+				}
+			}
+		}
+		/* find */
+		> div:nth-of-type(2) {
+			margin-left: auto;
+		}
+		/* search */
+		> div:nth-of-type(4) {
+			display: none;
+			@media (min-width: 1024px) {
+				display: block;
+			}
+		}
 		/* buttons */
 		> div:nth-of-type(5) {
-            display: none;
+			display: none;
 			@media (min-width: 768px) {
-                display: flex;
+				display: flex;
 				gap: 1rem;
 			}
 		}
 	}
 	> div:last-of-type {
-        padding: 1rem;
+		padding: 1rem;
 		display: flex;
 		align-items: center;
+		border-top: 2px solid var(--borderColor);
+		border-bottom: 2px solid var(--borderColor);
+		background-color: white;
+		text-transform: capitalize;
 		justify-content: center;
-        border-top: 2px solid black;
-		background-color: gray;
-        >div:first-of-type {
-            display: flex;
-            gap: 1rem;
-            @media (min-width: 768px) {
+		@media (min-width: 768px) {
+			background-color: var(--brandRed);
+			color: white;
+			border: none;
+		}
+		> div {
+			display: flex;
+			gap: 1rem;
+			&:first-of-type {
+				@media (min-width: 768px) {
+					display: none;
+				}
+			}
+            &:last-of-type {
                 display: none;
+                @media (min-width: 768px) {
+                    display: flex;
+                }
             }
-        }
-        ul {
-            display: none;
-            @media (min-width: 768px) {
-                display: flex;
-                gap: 4rem;
-            }
-        }
+		}
 	}
 `
 const Header = () => {
+	const links = [ 'menu', 'values', 'company', 'jobs' ]
+	const linksLong = [ 'view our menu', 'what we value', 'who we are', 'find jobs' ]
 	return (
 		<StyledHeader>
 			<div id="wrapper">
-				<div>find</div>
-				<div>logo</div>
-				<div>sign in</div>
-				<div>search</div>
 				<div>
-					<button>order pickup</button>
-					<button>order delivery</button>
+					<img src={logo} alt="" />
+					<ul>{links.map((link, i) => <li key={i}>{link}</li>)}</ul>
+					<ul>{linksLong.map((link, i) => <li key={i}>{link}</li>)}</ul>
+				</div>
+				<div>
+					<a href="/">
+						<BiMap />
+						<span>find</span>
+					</a>
+				</div>
+				<div>
+					<a href="/">
+						<BiUser />
+						<span>sign in</span>
+					</a>
+				</div>
+				<div>
+					<a href="/">
+						<BiSearch />
+						<span>search</span>
+					</a>
 				</div>
 			</div>
 			<div>
 				<div>
-                    <button>view our menu</button>
-                    <button>start an order</button>
-                </div>
-                <ul>
-                    <li>links</li>
-                    <li>links</li>
-                    <li>links</li>
-                    <li>links</li>
-                </ul>
+					<button>view our menu</button>
+					<button>start an order</button>
+				</div>
+				<div>
+					<button>order pickup</button>
+					<button>order delivery</button>
+				</div>
 			</div>
 		</StyledHeader>
 	)
--- src/globalStyles.js
+++ src/globalStyles.js
@@ -7,8 +7,13 @@ const GlobalStyles = createGlobalStyle`
         box-sizing: border-box;
         scrollbar-width: none;
     }
+    :root {
+        --brandBlue: #154C72;
+        --brandRed: #9F1B19;
+        --borderColor: #EBEBEB;
+    }
     body {
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-family: 'Rubik', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
         -webkit-font-smoothing: antialiased;
 	    -moz-osx-font-smoothing: grayscale;
     }
